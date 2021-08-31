f303fe904f83c55f8c0ca705d77cfce97983c3a6
jshi31 <j.shi@rochester.edu>
2021-08-31 00:33:58 -0400

    init discover60k dataset

--- train.py
+++ train.py
@@ -20,6 +20,14 @@ from util.visualizer import Visualizer
 
 if __name__ == '__main__':
     opt = TrainOptions().parse()   # get training options
+
+    # dataset: discover60k
+    opt.discover60k_kwargs = {
+        'input_dir': '/home/jshi31/dataset/discover60k/before',
+        'output_dir': '/home/jshi31/dataset/discover60k/after',
+        'anno_path': '/home/jshi31/dataset/discover60k/annotation/discover60k.json',
+    }
+
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
     dataset_size = len(dataset)    # get the number of images in the dataset.
     print('The number of training images = %d' % dataset_size)
