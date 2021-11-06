f197701e157410367c31a04b94c538a6a4552da9
jshi31 <j.shi@rochester.edu>
2021-11-06 15:56:39 -0400

    update lpips

--- options/test_options.py
+++ options/test_options.py
@@ -12,6 +12,7 @@ class TestOptions(BaseOptions):
         parser.add_argument('--sync', action='store_true', help='use the same latent code for different input images')
         parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio for the results')
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
+        parser.add_argument('--viz', action='store_true', help='visualize.')
 
         self.isTrain = False
         return parser
